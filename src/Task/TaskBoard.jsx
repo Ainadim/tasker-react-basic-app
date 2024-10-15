0827dcc1f5610d6c561ae5069bba69be5bde649f
Arafin Islam <nadimface@gmailcom>
2024-10-15 10:01:34 +0600

    Add new task

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
-  const taskDataList = {
+  const defaultTask = {
     id: crypto.randomUUID(),
     title: "Learn React",
     description:
@@ -14,21 +15,25 @@ const TaskBoard = () => {
     isFavorite: false,
   };
 
-  const [tasks, setTasks] = useState([taskDataList]);
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
 
-  function handleAddClickButton() {
-    console.log("object");
+  function handleAddTask(newTask) {
+    console.log(newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
   }
 
   return (
     <>
       <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal onSave={handleAddTask} />}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask></SearchTask>
           </div>
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskAction onAddClick={handleAddClickButton}></TaskAction>
+            <TaskAction onAddClick={() => setShowAddModal(true)}></TaskAction>
             <TaskList tasks={tasks}></TaskList>
           </div>
         </div>
